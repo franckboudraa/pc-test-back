668873b419d10dc8e92811ba954dd919736d6029
neocydfr <franckboudraa@gmail.com>
2018-03-16 11:40:58 +0100

    created logger for sql queries

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const readFile = (path, opts = 'utf8') =>
     });
   });
 
+// Create a stream for logging SQL queries
+const stream = fs.createWriteStream(path.join(__dirname, 'logs/queries.log'), {
+  flags: 'a'
+});
+
 app.get('/:model/:method', async (req, res) => {
   const { model, method } = req.params;
 
@@ -22,6 +27,7 @@ app.get('/:model/:method', async (req, res) => {
     const data = await readFile(
       path.join(__dirname, `assets/sql/${model}/${method}.sql`)
     );
+    stream.write(data + '\n'); // Log each query
     res.send('ok');
   } catch (error) {
     res.status(500).send('An error occured');
